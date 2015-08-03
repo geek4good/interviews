fc51d9004b8faf8a314893fe6ff3cde5c2afc00c
Lucas Mbiwe <lucas.mbiwe@mendeley.com>
2015-08-03 12:57:17 +0100

    Don't use 'let' statements in the test setup

--- test_result/test_result_spec.rb
+++ test_result/test_result_spec.rb
@@ -3,20 +3,23 @@ require "rspec"
 require "./test_result"
 
 describe TestResult do
-  let(:folder) { "timestamp" }
-  let(:basenames) { %W(01_unit_tests 02_functional_tests 03_integration_tests) }
-
   before(:all) do
+    folder = "timestamp"
+    basenames = %W(01_unit_tests 02_functional_tests 03_integration_tests)
+    outcomes = %W(success success failure)
+
     FileUtils.mkdir_p(folder)
     basenames.each_with_index do |basename, index|
       filename = File.join(folder, "#{basename}.txt")
-      File.write(filename, index == 2 ? "failure" : "success")
+      File.write(filename, outcomes[index])
     end
   end
 
   after(:all) do
-    FileUtils.rm_rf()
+    FileUtils.rm_rf("timestamp")
   end
+
+  subject(:test_result) { TestResult.new }
 
   describe "#to_hash" do
     # => { "01_unit_tests" => "success", "02_functional_tests" => "success", "03_integration_tests" => "failure" }
