857a09e0d8f1381209dd061b48d88c50072217af
Lucas Mbiwe <lucas.mbiwe@mendeley.com>
2015-09-10 10:36:44 +0100

    Add array challenge

---
+++ array_challenge/array_challenge.rb
@@ -0,0 +1,7 @@
+class ArrayChallenge
+  attr_reader :arrays
+
+  def initialize(*arrays)
+    arrays = arrays
+  end
+end
---
+++ array_challenge/array_challenge_spec.rb
@@ -0,0 +1,25 @@
+require "./array_challenge"
+
+describe ArrayChallenge do
+  subject(:challenge) { described_class.new(arrays) }
+
+  describe "#captilize_last_letter" do
+    let(:arrays) { %w(ant bee cow) }
+
+    it "capitalizes the last letter of each word in the collection" do
+      result = challenge.capitalize_last_letter
+
+      expect(result).to eq(%w(anT beE coW))
+    end
+  end
+
+  describe "#chars_unique_to_either_collection" do
+    let(:arrays) { [%w(ant bee cat), %w(bat eel fox rat)] }
+
+    it "returns the chars that only exists in either of the arrays" do
+      result = challenge.chars_unique_to_either_collection
+
+      expect(result).to eq(%w(a n t b c d o g l f o x))
+    end
+  end
+end
